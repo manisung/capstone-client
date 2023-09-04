f1f5a4d50408dbd0e6b18ad794f4c5c3204beb99
Elise <eliselimin@outlook.com>
2023-09-03 18:05:29 -0700

    added anitmation to landing page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,18 @@
 import "./Hero.scss";
-import heroPhoto from "../../assets/images/hero.png"
+import landingVideo from "../../assets/videos/landingvideo-v.MOV"
+// import landingVideo from "../../assets/videos/landing-video.MP4"
+
+
+
 
 
 function Hero() {
+
     return(
         <div className="hero">
-        <img className="hero__image" src={heroPhoto} alt="Yumi Chung Cover Photo"/>
+        {/* <img className="hero__image" src={heroPhoto} alt="Yumi Chung Cover Photo"/> */}
+        <video src={landingVideo}  id="landing-video" className="hero__video" type="video/mp4" controls muted autoPlay loop preload="auto"></video>
+        <div id="text" className="hero__video--welcome">Welcome to <br/>Yumi Chung<br/>Official Website</div>
         </div>
     )
 }
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -9,7 +9,7 @@ function AboutPage() {
         <div className="about__title">About</div>
         <div className="about__content-wrapper">
         <div className="about__sub-title">Early Childhood</div>
-        <div className="about__image-wrapper"><img className="about__image-1" src={childhoodPic} alt="Yumi's childhood recording picture" /></div>
+        <div className="about__image-wrapper"><img className="about__image-1" src={childhoodPic} alt="Yumi's childhood recording" /></div>
         
         <div className="about__description">On October 5, 2014, Yumi, who was only 7 years old, won the children's vocal group in 
         the "4th China Children's Little Golden Bell Music Competition and the 2nd 
@@ -17,11 +17,11 @@ function AboutPage() {
         She also joined the girl group Honey Bees, and followed Kayan Sit to perform on stage, but later 
         left Honey Bees and joined A Little Bit Shy with six girls at the age of 10.</div>
         <div className="about__sub-title">Competition Period</div>
-        <div className="about__image-wrapper"><img className="about__image-2" src={wildCity} alt="Yumi's Star Academy competiton picture" /></div>
+        <div className="about__image-wrapper"><img className="about__image-2" src={wildCity} alt="Yumi's Star Academy competiton" /></div>
         <div className="about__description">On January 10, 2022, Yumi sang Aaron Kwok's song "Mood for the Fourth Night" in the final of "Legend of Dreams" and became her fourth song to reach 1 million hits in the competition.
 </div>
 <div className="about__sub-title">Debut</div>
-        <div className="about__image-wrapper"><img className="about__image-3" src={debut} alt="Yumi's new music video picture mosaic" /></div>
+        <div className="about__image-wrapper"><img className="about__image-3" src={debut} alt="Yumi's new music video mosaic" /></div>
         <div className="about__description">On February 4, 2022, Yumi released her first solo single "Breakin' My Heart", which was distributed to major electronic media, officially debuting as a solo artist. The song was released the next day, and she became the first member to release a solo single under the name of an After Class member. After being sent to Taiwan, this song became the champion song of two stations, and achieved two championships, one second and one season.</div>
         <div className="about__description">On October 27, Yumi released her second solo single "wanna be close to u", which 
         won one championship and two seasons in major electronic media. At the end of the same year, Yumi was shortlisted for the
