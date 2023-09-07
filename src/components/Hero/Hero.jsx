940217861cf1a9af778694ee1868e41eb4fc55c9
Elise <eliselimin@outlook.com>
2023-09-06 21:33:30 -0700

    edited the height of hero to auto

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,10 @@
 import "./Hero.scss";
 import landingVideo from "../../assets/videos/landingvideo-v.MOV"
-// import landingVideo from "../../assets/videos/landing-video.MP4"
-
-
-
-
 
 function Hero() {
 
     return(
         <div className="hero">
-        {/* <img className="hero__image" src={heroPhoto} alt="Yumi Chung Cover Photo"/> */}
         <video src={landingVideo}  id="landing-video" className="hero__video" type="video/mp4" controls muted autoPlay playsInline loop preload="auto"></video>
         <div id="text" className="hero__video--welcome">Welcome to <br/>Yumi Chung<br/>Official Website</div>
         </div>
